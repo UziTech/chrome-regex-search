79fcadfb6dcd68c60cbd1f717fdf8b62930e2b7c
Tony Brix <Tony@Brix.ninja>
2017-05-25 17:11:03 -0500

    format

--- src/js/background.js
+++ src/js/background.js
@@ -1,9 +1,10 @@
+/* globals chrome */
 /* Received returnSearchInfo message, set badge text with number of results */
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if ('returnSearchInfo' == request.message) {
-    chrome.browserAction.setBadgeText({
-      'text': String(request.numResults),
-      'tabId': sender.tab.id
-    });
-  }
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+	if ("returnSearchInfo" === request.message) {
+		chrome.browserAction.setBadgeText({
+			"text": String(request.numResults),
+			"tabId": sender.tab.id
+		});
+	}
 });
